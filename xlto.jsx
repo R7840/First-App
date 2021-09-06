3d40db7de2c856bdb8de761cc49fe0daf21baf2c
rahul goyal <39808226+R7840@users.noreply.github.com>
2021-09-06 23:42:26 +0530

    Update xlto.jsx

--- xlto.jsx
+++ xlto.jsx
@@ -9,21 +9,22 @@ export default class Xlto extends Component {
     this.state = { data: this.props.prod, bigdata: [], filt: this.props.filt };
   }
 
-  // using filtered output then getting the data
   ExportToExcel = (data) => {  
     exportFromJSON({ data, fileName, exportType })  
   }
   render() {
     var b = [],
       j = 0;
-    // copying the filtered output
     this.state.data.map((val) => {
-      if (val.Category === this.state.filt) {
+     if ((val.Category === this.state.filt)||(val.Cors === this.state.filt)||(val.HTTPS === this.state.filt)) {
         b[j] = val;
         ++j;
       }
       return "";
     });
+
+    console.log("filter json", this.state.bigdata);
+
     return (
       <div
         className="btn btn-primary"
@@ -31,13 +32,17 @@ export default class Xlto extends Component {
           this.setState({ bigdata: b });
         }}
         onClick={() => {
-          this.ExportToExcel(this.state.bigdata)
+          this.ExportToExcel(this.state.bigdata);
+          this.setState({ bigdata: [] });
           window.location.reload();
         }}
       >
         filter save
       </div>
     );
+  }
+  componentWillUnmount() {
+    console.log("bye bye");
   }
   
 }
